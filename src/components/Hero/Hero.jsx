fd7a033d26b5d89c2151f0d1355e8f59e41faf28
SHAIKH FAISAL <89987477+skffaisal@users.noreply.github.com>
2024-05-07 06:38:47 +0530

    comitting

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm Ada</h1>
+        <h1 className={styles.title}>Hi, I'm Mike</h1>
         <p className={styles.description}>
           I'm a full-stack developer with 5 years of experience using React and
           NodeJS. Reach out if you'd like to learn more!
@@ -21,8 +21,7 @@ export const Hero = () => {
         alt="Hero image of me"
         className={styles.heroImg}
       />
-      <div className={styles.topBlur} />
-      <div className={styles.bottomBlur} />
+
     </section>
   );
 };
